3cf541d74e8c0c42bb4e6414c8474478e214812c
Primoz Cigler <primoz@proteusnet.com>
2017-06-28 09:25:33 +0200

    Add JS logic for permanent setting

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -10,7 +10,21 @@
 
 		// Go ahead and setup references to the plugin menu item and all admin notices.
 		var $admin_button = $( '#wp-admin-bar-toggle-admin-notices a' ),
-			$messages     = $( '.notice, .warning, .error' );
+			$messages     = $( '.notice, .warning, .error' ),
+			currentState  = localStorage.getItem( 'toggleAdminNoticesState' );
+
+		if ( null === currentState ) {
+			currentState = 'show';
+			localStorage.setItem( 'toggleAdminNoticesState', currentState );
+		}
+
+		/**
+		 * Toggle the state and save the setting to the local storage
+		 */
+		var toggleState = function toggleState() {
+			currentState = 'show' === currentState ? 'hide' : 'show';
+			localStorage.setItem( 'toggleAdminNoticesState', currentState );
+		}
 
 		/**
 		 * Hide all of the messages when the option is include and restore the menu items'
@@ -21,6 +35,15 @@
 
 			$messages.toggle( 'medium' );
 			$( this ).trigger( 'blur' );
-		});
+
+			toggleState();
+		} );
+
+		/**
+		 * Permanently hidden
+		 */
+		if ( 'hide' === currentState ) {
+			$messages.hide();
+		}
 	});
 })( jQuery );
